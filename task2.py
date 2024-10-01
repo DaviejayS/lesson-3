7a249d3e1400279d71aee53f58125513423144e3
DaviejayS <vitorinocapito@gmail.com>
2024-10-01 11:37:16 +0800

    kapoya uy

---
+++ task2.py
@@ -0,0 +1,19 @@
+myfavfruitsnum = int(input("pila akong ganahan na fruits"))
+myfavfruits = []
+
+for i in range(myfavfruitsnum):
+    fruits = input("Enter name fruits")
+    myfavfruits.append(fruits)
+
+print(myfavfruit)
+
+for data in myfavfruits:
+    if data == "banana":
+        break
+    elif data == "apple":
+        print("happy eating")
+
+
+
+
+    
